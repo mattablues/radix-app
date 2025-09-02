fa144c6acef1663e2864f02edf8525e9d51c0e13
Mats Åkebrand <malle@akebrands.se>
2025-09-02 14:24:32 +0200

    removed messages from side navbar

--- views/layouts/admin.ratio.php
+++ views/layouts/admin.ratio.php
@@ -102,40 +102,6 @@
 {% endif; %}
           </ul>
         </div>
-
-        <div x-data="{ sidebarDropdown: false }">
-          <div class="mt-2 px-4 flex items-center rounded-md transition-all duration-300 cursor-pointer hover:bg-blue-600">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-              <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-              <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-            </svg>
-
-            <div class="w-full flex justify-between items-center" x-on:click="sidebarDropdown = !sidebarDropdown">
-              <span class="text-[15px] ml-4 py-3 text-gray-200">Meddelanden</span>
-              <span class="text-sm rotate-180" id="arrow">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"
-                  :class="sidebarDropdown ? 'transition-all rotate-180 duration-300 ease-out' : 'transition-all rotate-0 duration-300 ease-out'"
-                >
-                  <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clip-rule="evenodd" />
-                </svg>
-              </span>
-            </div>
-          </div>
-          <ul
-            x-show="sidebarDropdown"
-            x-transition:enter="transition-all duration-200 ease-out"
-            x-transition:enter-start="-translate-y-5 opacity-0"
-            x-transition:enter-end="translate-y-0 opacity-100"
-            x-transition:leave="transition-all duration-200 ease-in"
-            x-transition:leave-start="translate-y-0 opacity-100"
-            x-transition:leave-end="-translate-y-5 opacity-0"
-            class="leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto border-l-1 border-gray-700"
-          >
-            <li class="mt-1 ml-1 rounded-md cursor-pointer hover:bg-gray-700">
-              <a href="" class="w-full inline-block py-2 px-8">Chat</a>
-            </li>
-          </ul>
-        </div>
 {% if($currentUser->hasRole('admin')) : %}
         <hr class="my-4 text-gray-400">
         <div x-data="{ sidebarDropdown: false }">
