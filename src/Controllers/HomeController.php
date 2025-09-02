6dd11bfd5d92c1c5bba67b4fef0b4d48e0e23308
Mats Åkebrand <malle@akebrands.se>
2025-09-02 14:12:17 +0200

    update home controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -9,50 +9,8 @@ use Radix\Http\Response;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Definiera anpassade filter för HomeController
-     */
-//    protected function filters(): array
-//    {
-//        return [
-//            'lowercase' => [
-//                'callback' => fn($value) => strtolower($value),
-//                'type' => 'string',
-//            ],
-//            'reverse' => [
-//                'callback' => fn($value) => strrev($value),
-//                'type' => 'string',
-//            ],
-//        ];
-//    }
-
     public function index(): Response
     {
-        // Dynamiskt registrera filter endast för denna vy
-//        $this->viewer->registerFilter('lowercase', fn($value) => strtolower($value));
-//        $this->viewer->registerFilter('reverse', fn($value) => strrev($value));
-        // Rendera och returnera vyn
-//        return $this->view('home.index', ['test' => 'Hello World']);
-
-//          $user = User::find(2, true);
-//          $user->restore();
-//
-//       $users = User::with('status')->paginate();
-//          $users = User::paginate();
-//          dd($users);
-//
-//          $softDeletedUsers = User::getOnlySoftDeleted()->get();
-//          dd($softDeletedUsers);
-//
-//          $user = User::with('status')->search('åkebrand', ['last_name', 'first_name']);
-//
-//          dd($user);;
-
-//   $user = User::find(1);
-//   dd($user->status()->first());
-
-//   dd(User::with('status')->get());
-
-          return $this->view('home.index');
+        return $this->view('home.index');
     }
 }
