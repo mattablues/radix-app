19ac379525c0ec74867f3d89b723c6f578e103b1
Mats Åkebrand <malle@akebrands.se>
2025-09-25 00:25:45 +0200

    add geo locator

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -6,11 +6,21 @@ namespace App\Controllers;
 
 use Radix\Controller\AbstractController;
 use Radix\Http\Response;
+use Radix\Support\GeoLocator;
 
 class HomeController extends AbstractController
 {
     public function index(): Response
     {
+//        $geoLocator = new GeoLocator();
+//
+//        $location = $geoLocator->getLocation(); // Hämta plats för besökaren
+//        echo "Land: " . ($location['country'] ?? 'Okänt');
+//        echo "Stad: " . ($location['city'] ?? 'Okänt');
+//
+//        // Hämta endast specifik data
+//        $country = $geoLocator->get('country', '85.228.5.49'); // För valfri IP
+//        echo "Land för 85.228.5.49: $country";
         return $this->view('home.index');
     }
 }
