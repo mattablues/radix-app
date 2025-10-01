9e52d25f02943ac9fbee9f75e9ad50a07fd0312a
Mats Åkebrand <malle@akebrands.se>
2025-10-01 15:40:50 +0200

    add string helper

---
+++ config/pluralization.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'irregular' => [
+        'categories' => 'category',
+        'children' => 'child',
+        'people' => 'person',
+        'men' => 'man',
+        'women' => 'woman',
+        'indices' => 'index',
+        'appendices' => 'appendix',
+        'data' => 'datum',
+        'status' => 'status', // Lägg till 'status' som undantag eftersom det är oregelbundet
+        // Lägg till fler undantag här vid behov
+    ],
+];
