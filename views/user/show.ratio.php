36ae95fe780a9b6078eea28bd40e8c88a0e2d0f4
Mats Åkebrand <malle@akebrands.se>
2025-10-22 10:43:25 +0200

    update show

--- views/user/show.ratio.php
+++ views/user/show.ratio.php
@@ -6,31 +6,53 @@
     <section x-data="{ openRoleModal: false, selectedRole: '{{ $user->fetchGuardedAttribute('role') }}' }">
       <h1 class="text-3xl mb-8">Konto</h1>
 {% if($user) : %}
-      <p class="text-gray-700 text-sm">Namn: {{ $user->getAttribute('first_name') }} {{ $currentUser->getAttribute('last_name') }}</p>
-      <p class="text-gray-700 text-sm">E-post: {{ $user->getAttribute('email') }}</p>
-      {% if($currentUser->hasAtLeast('moderator')) : %}
-      <p class="text-gray-700 text-sm">kontostatus: <span class="inline-block text-xs font-semibold py-0.5 px-1.5 rounded {{ $user->getRelation('status')->getAttribute('status') }}">{{ $user->getRelation('status')->translateStatus($user->getRelation('status')->getAttribute('status')) }}</span></p>
-      {% endif; %}
-      {% if($user->getRelation('status')->getAttribute('active_at')) : %}
-      <p class="text-gray-700 text-sm">Senast aktiv: {{ $datetime->frame($user->getRelation('status')->getAttribute('active_at')) }}</p>
-      {% else : %}
-      <p class="text-gray-700 text-sm">Senast aktiv: aldrig</p>
-      {% endif; %}
-      {% if($currentUser->isAdmin() && !$user->isAdmin()) : %}
-      <div class="mt-6">
-        <div class="flex items-center gap-2 mb-4">
-          <span class="text-sm text-gray-700">Behörighet:</span>
-          <span class="inline-block text-xs font-semibold bg-gray-100 text-gray-800 py-0.5 px-1.5 rounded">{{ $user->fetchGuardedAttribute('role') }}</span>
+      <div class="w-full md:max-w-1/2 flex justify-between border border-gray-200 rounded px-5 py-3">
+        <ul>
+          <li class="flex items-start gap-2 my-1">
+            <span class="shrink-0 inline-block w-28 text-sm font-semibold text-gray-700">Namn:</span>
+            <span class="inline-block text-sm text-gray-700">{{ $user->getAttribute('first_name') }} {{ $currentUser->getAttribute('last_name') }}</span>
+          </li>
+          <li class="flex items-start gap-2 my-1">
+            <span class="shrink-0 inline-block w-28 text-sm font-semibold text-gray-700">E-post:</span>
+            <span class="inline-block text-sm text-gray-700">{{ $user->getAttribute('email') }}</span>
+          </li>
+          <li class="flex items-start gap-2 my-1">
+            <span class="shrink-0 inline-block w-28 text-sm font-semibold text-gray-700">Senast aktiv:</span>
+          {% if($user->getRelation('status')->getAttribute('active_at')) : %}
+            <span class="inline-block text-sm text-gray-700">
+              {{ $datetime->frame($user->getRelation('status')->getAttribute('active_at')) }}
+            </span>
+          {% else : %}
+            <span class="inline-block text-sm text-gray-700">aldrig</span>
+          {% endif; %}
+          </li>
+          <li class="flex items-start gap-2 my-1">
+          {% if($currentUser->hasAtLeast('moderator')) : %}
+            <span class="shrink-0 inline-block w-28 text-sm font-semibold text-gray-700">Kontostatus:</span>
+            <span class="inline-block text-xs font-semibold py-0.5 px-1.5 rounded {{ $user->getRelation('status')->getAttribute('status') }}">{{ $user->getRelation('status')->translateStatus($user->getRelation('status')->getAttribute('status')) }}</span>
+          {% endif; %}
+          </li>
+          {% if($currentUser->isAdmin() && !$user->isAdmin()) : %}
+          <li class="flex items-start gap-2 my-1">
+            <span class="shrink-0 inline-block w-28 text-sm font-semibold text-gray-700">Behörighet:</span>
+            <span class="inline-block text-xs font-semibold bg-gray-100 text-gray-800 py-0.5 px-1.5 rounded">{{ $user->fetchGuardedAttribute('role') }}</span>
+          </li>
+          {% endif; %}
+        </ul>
+        <div class="flex flex-col items-center justify-between gap-2 mb-1">
+          <img src="{{ versioned_file($user->getAttribute('avatar'), '/images/graphics/avatar.png') }}" alt="Avatar {{ $user->getAttribute('first_name') }} {{ $user->getAttribute('last_name') }}" class="w-[70px] h-[70px] rounded-full object-cover">
+{% if($currentUser->isAdmin() && !$user->isAdmin()) : %}
+          <button
+              type="button"
+              x-on:click="openRoleModal = true"
+              class="text-xs font-semibold bg-blue-600 text-white py-0.5 px-1.5 rounded cursor-pointer hover:bg-blue-700 transition-colors duration-300"
+            >
+              ändra behörighet
+            </button>
+{% endif; %}
         </div>
-        <button
-          type="button"
-          x-on:click="openRoleModal = true"
-          class="text-sm font-semibold bg-blue-600 text-white py-0.5 px-2 rounded cursor-pointer hover:bg-blue-700 transition-colors duration-300"
-        >
-          Ändra behörighet
-        </button>
       </div>
-
+      {% if($currentUser->isAdmin() && !$user->isAdmin()) : %}
       <div
         x-show="openRoleModal"
         x-cloak
