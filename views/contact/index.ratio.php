eab7f5c47872ddf9cfb3be0912f912027481b02f
Mats Åkebrand <malle@akebrands.se>
2025-10-27 11:00:36 +0100

    minor fix

--- views/contact/index.ratio.php
+++ views/contact/index.ratio.php
@@ -68,7 +68,7 @@
               <div class="flex gap-2 items-center">
                 <button class="text-sm whitespace-nowrap py-2 px-3 border border-blue-600 bg-blue-600 hover:bg-blue-700  transition-all duration-300 text-white rounded-lg cursor-pointer">Skicka</button>
                 {% if (error($errors, 'form-error')) : %}
-                <span class="block left-1 right-1 absolute top-16 text-xs text-red-600 leading-3.5">{{ error($errors, 'form-error') }}</span>
+                <span class="block left-1 right-1 absolute top-12 text-xs text-red-600 leading-3.5">{{ error($errors, 'form-error') }}</span>
                 {% endif %}
               </div>
             </div>
