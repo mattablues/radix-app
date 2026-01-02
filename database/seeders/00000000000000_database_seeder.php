f8dc360ad5bce47cbaf378caaecbc690dd730102
mats <mats@akebrands.se>
2026-01-02 14:29:24 +0100

    update seeder

--- database/seeders/00000000000000_database_seeder.php
+++ database/seeders/00000000000000_database_seeder.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 final readonly class DatabaseSeeder
 {
-    public function __construct(private \PDO $pdo) {}
-
     public function run(): void
     {
-        (new UsersSeeder($this->pdo))->run();
-        (new StatusSeeder($this->pdo))->run();
+        (new UsersSeeder())->run();
+        (new StatusSeeder())->run();
     }
 
     public function down(): void
     {
         // Viktigt: ta bort barn först
-        (new StatusSeeder($this->pdo))->down();
-        (new UsersSeeder($this->pdo))->down();
+        (new StatusSeeder())->down();
+        (new UsersSeeder())->down();
     }
 }
