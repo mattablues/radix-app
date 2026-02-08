bf39a32ef02d3b111cbec9ccf14840ace3d77c5e
Mats Åkebrand <malle@akebrands.se>
2026-02-08 18:34:35 +0100

    Remove framework folder; framework is now separate repo

--- framework/src/Container/Argument/LiteralArgumentInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Radix\Container\Argument;
-
-interface LiteralArgumentInterface extends ArgumentInterface {}
--- framework/src/Container/Exception/ContainerConfigException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Radix\Container\Exception;
-
-use Exception;
-
-class ContainerConfigException extends Exception {}
