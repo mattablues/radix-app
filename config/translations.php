2f4b15f5cc55463148b3f178baeba1e374335172
mattablues <malle@akebrands.se>
2026-02-13 11:45:12 +0100

    add translation config file

---
+++ config/translations.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'translations' => [
+        'validations' => [
+            'name' => 'namn',
+            'first_name' => 'förnamn',
+            'last_name' => 'efternamn',
+            'email' => 'e-post',
+            'message' => 'meddelande',
+            'password' => 'lösenord',
+            'password_confirmation' => 'repetera lösenord',
+            'honeypot' => 'honeypot',
+            'category' => 'kategori',
+            'description' => 'beskrivning',
+            'current_password' => 'nuvarande lösenord',
+        ],
+    ],
+];
