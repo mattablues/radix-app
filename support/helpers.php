b6a99ef996164f757310d8e4c9dddf5e3d6415dc
Mats Åkebrand <malle@akebrands.se>
2026-02-22 18:46:50 +0100

    Fix redirects: check named route instead of route file

--- support/helpers.php
+++ support/helpers.php
@@ -1,3 +1,10 @@
 <?php
 
 declare(strict_types=1);
+
+if (!function_exists('route_exists')) {
+    function route_exists(string $name): bool
+    {
+        return array_key_exists($name, \Radix\Routing\Router::routeNames());
+    }
+}
