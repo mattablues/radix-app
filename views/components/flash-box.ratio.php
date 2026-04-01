42fb8a5388320dd48dab3bbf6eb58470037e75c4
Mats Åkebrand <malle@akebrands.se>
2026-04-01 21:28:10 +0200

    refactor flash messages

--- views/components/flash-box.ratio.php
+++
@@ -1,8 +0,0 @@
-{% if ($session->has('flash_notification')) : %}
-    {% $message = $session->flashMessage(); %}
-{% endif; %}
-{% if (isset($message) && is_array($message)) : %}
-  <div class="animate-fade-in-top">
-    <p class="flash-box flash-box-{{ $message['type'] }} text-xs">{{ $message['body'] }}</p>
-  </div>
-{% endif; %}
